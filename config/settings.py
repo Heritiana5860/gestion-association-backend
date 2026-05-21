f0dd6e7e33faf37b7477c8e029b6d75207f09a63
unknown <Admin-IT@LT-ADES-79.ades-solaire.mg>
2026-05-21 09:38:39 +0300

    Amelioration de docker compose

--- config/settings.py
+++ config/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = 'django-insecure-ro**+#0_xi_@*i(0!dirp%id7rm!rbk#c2u()gi)w*41z!1e@^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ['10.85.5.43']
-
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,10.85.5.37").split(",")
 
 # Application definition
 
@@ -92,19 +91,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-DATABASES = {
-    'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'HOST': os.environ.get('DB_HOST', 'db'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
