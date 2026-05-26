202c2d3f4c50a57fe082dc8373a79eabbf3e71c8
unknown <Admin-IT@LT-ADES-79.ades-solaire.mg>
2026-05-26 16:28:44 +0300

    Member serializer

--- info/serializers/member_serializer.py
+++ info/serializers/member_serializer.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
-from ..models import Member
+from ..models import Member, Cotisation
+
+class CotisationInlineSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cotisation
+        fields = ['id', 'year', 'amount', 'is_paid', 'payment_date']
 
 class MemberSerializer(serializers.ModelSerializer):
     created_at = serializers.DateField(read_only=True)
+    cotisations = CotisationInlineSerializer(many=True, read_only=True)
     
     class Meta:
         model = Member
-        fields = ['id', 'full_name', 'number_phone', 'is_inside', 'cde', 'address', 'school', 'level', 'statut', 'created_at']
+        fields = ['id', 'full_name', 'number_phone', 'is_inside', 'cde', 'address', 'school', 'level', 'statut', 'created_at', 'cotisations']
