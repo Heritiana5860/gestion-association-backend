13733cebb6dea4d3b649cc2b875adddb3a8cba36
unknown <Admin-IT@LT-ADES-79.ades-solaire.mg>
2026-07-08 15:18:33 +0300

    Creation de superuser

---
+++ create_admin.py
@@ -0,0 +1,21 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+if not all([username, email, password]):
+    print("Variables d'environnement pour le superuser manquantes. Création annulée.")
+else:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+        print(f"Superuser '{username}' créé avec succès !")
+    else:
+        print(f"Le superuser '{username}' existe déjà.")
