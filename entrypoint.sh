077bf07d2032d7100d3e7b40f9fe26335f2641c6
unknown <Admin-IT@LT-ADES-79.ades-solaire.mg>
2026-07-08 15:51:15 +0300

    migrations

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,8 @@ set -e
 echo "=== Application des migrations Django ==="
 python manage.py migrate --noinput
 
+echo "=== Création du Superuser ==="
+python create_admin.py
+
 echo "=== Démarrage de Gunicorn ==="
 exec gunicorn config.wsgi:application --bind 0.0.0.0:8000
