d1e5b79add9fef6e1a44d82903573b36d3ba9fd3
unknown <Admin-IT@LT-ADES-79.ades-solaire.mg>
2026-07-11 23:05:29 +0300

    admin auth

--- app_auth/admin.py
+++ app_auth/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
+from django.contrib.auth.admin import UserAdmin, GroupAdmin
+from unfold.admin import ModelAdmin
 
-# Register your models here.
+admin.site.unregister(User)
+admin.site.unregister(Group)
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin, ModelAdmin):
+    pass
+
+@admin.register(Group)
+class CustomGroupAdmin(GroupAdmin, ModelAdmin):
+    pass
