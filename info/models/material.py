5f9f93910204feb07aea70292d472f58b6620861
unknown <Admin-IT@LT-ADES-79.ades-solaire.mg>
2026-07-16 09:30:52 +0300

    rectification

--- info/models/material.py
+++ info/models/material.py
@@ -3,7 +3,7 @@ from django.db import models
 class Material(models.Model):
     nom = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
-    nombre_materiel = models.IntegerField(blank=True, null=True)
+    nombre_materiel = models.IntegerField(blank=True, null=True, default=0)
     
     created_at = models.DateField(auto_now_add=True)
     is_update = models.DateField(auto_now=True)
--- info/serializers/material_serializer.py
+++ info/serializers/material_serializer.py
@@ -7,4 +7,4 @@ class MaterialSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Material
-        fields = ['nom', 'description', 'created_at', 'is_update']
+        fields = ['nom', 'description', "nombre_materiel", 'created_at', 'is_update']
