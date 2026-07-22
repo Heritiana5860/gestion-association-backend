bb68478826dff7686974d25a3bc85f86a4756c82
unknown <Admin-IT@LT-ADES-79.ades-solaire.mg>
2026-07-22 09:46:34 +0300

    fixed register

--- app_auth/views/register_view.py
+++ app_auth/views/register_view.py
@@ -1,9 +1,11 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import AllowAny
+
+User = get_user_model()
 
 class RegisterView(APIView):
     permission_classes = [AllowAny]
