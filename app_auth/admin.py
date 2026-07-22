530a1b7f5b2e151b0cb788b4c01ed9052592edad
unknown <Admin-IT@LT-ADES-79.ades-solaire.mg>
2026-07-22 08:37:37 +0300

    auth admin correction

--- app_auth/admin.py
+++ app_auth/admin.py
@@ -1,15 +1,22 @@
 from django.contrib import admin
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin, GroupAdmin
+from django.contrib.auth import get_user_model
 from unfold.admin import ModelAdmin
 
-admin.site.unregister(User)
-admin.site.unregister(Group)
+# Récupère votre CustomUser configuré dans AUTH_USER_MODEL
+User = get_user_model()
 
-@admin.register(User)
-class CustomUserAdmin(UserAdmin, ModelAdmin):
-    pass
+admin.site.unregister(Group)
 
 @admin.register(Group)
 class CustomGroupAdmin(GroupAdmin, ModelAdmin):
     pass
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin, ModelAdmin):
+    # Permet de voir et modifier le champ 'role' dans l'interface Unfold
+    fieldsets = UserAdmin.fieldsets + (
+        ("Rôles & Permissions", {"fields": ("role",)}),
+    )
+    list_display = ("username", "email", "first_name", "last_name", "role", "is_staff")
